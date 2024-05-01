7ca818feac4fa56f4736255070bd756b5c66394f
sai-nikshay93269 <168342982+sai-nikshay93269@users.noreply.github.com>
2024-05-01 16:15:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!! new change at 04:04pm"); 
+        System.out.println("Hello, World!!"); 
     }
 }
